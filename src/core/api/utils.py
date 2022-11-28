41d38a121bef9ff29a011f399cabbe165fa919c6
Jester6136 <bagiangson2001@gmail.com>
2022-11-28 21:45:19 +0700

    update

--- src/core/api/utils.py
+++ src/core/api/utils.py
@@ -99,7 +99,6 @@ def find_best_matching_in_dict(query, values_dict):
 def remove_dup(result_list):
     return list(dict.fromkeys(result_list))
 
-
 def search_in_database(user_token):
     df = pd.read_csv(DATA_CSV)
     procedures = df[df.procedure_name.str.contains(user_token, na=False)]
@@ -107,7 +106,6 @@ def search_in_database(user_token):
     procedure_list = list(chain.from_iterable(procedures.values.tolist()))
     procedure_list = sorted(procedure_list, key=len)
     return procedure_list
-
 
 def ranking_result(user_sent: str):
     values_dict = load_dict_values(DATA_TXT)
